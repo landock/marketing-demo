a044b06268b794551af77b3d0b318ef50a62a1ec
Alan Mabry <alan.mabry@gmail.com>
2018-10-29 19:11:05 -0400

    omega bubbles - wip

---
+++ source/javascripts/BubbleAnimation.js
@@ -0,0 +1,40 @@
+import scrollama from 'scrollama';
+
+function handleStepEnter(response) {
+  const bubbles = $(response.element).find('.omega-bubble');
+
+  if (bubbles.length && bubbles.length === 1) {
+    bubbles.addClass('fadeInUp animated');
+  } else {
+    console.log('bubble.length', bubbles.length);
+
+    $(bubbles[0]).addClass('fadeInUp animated');
+    setTimeout(function(){
+        $(bubbles[1]).addClass('fadeInUp animated');
+    }, 250);
+    setTimeout(function(){
+        $(bubbles[2]).addClass('fadeInUp animated');
+    }, 500);
+  }
+}
+
+function handleStepExit(response) {
+  $(response.element).find('.omega-bubble').addClass('fadeOutDown');
+
+  setTimeout(function() {
+    $(response.element).find('.omega-bubble').removeClass('fadeOutDown fadeInUp animated');
+  }, 500);
+}
+
+function BubbleAnimation() {
+  this.scroller = scrollama();
+
+ this.scroller
+    .setup({
+      step:'.omega-bubble-wrapper'
+    })
+    .onStepEnter(handleStepEnter)
+    .onStepExit(handleStepExit);
+}
+
+export default BubbleAnimation;

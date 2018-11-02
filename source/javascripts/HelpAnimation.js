f682d176dfcb879bf06ca2de8154371671b91322
Alan Mabry <alan.mabry@gmail.com>
2018-11-02 16:24:13 -0400

    text animations

--- source/javascripts/HelpAnimation.js
+++ source/javascripts/HelpAnimation.js
@@ -1,5 +1,9 @@
 import scrollama from 'scrollama';
 
+function handleStepEnter(response) {
+  $('.omega-help-subtitle').addClass('animated fadeInDown');
+}
+
 function HelpAnimation() {
   this.timeline = new TimelineMax({paused: true})
     .from('.last', 2000, {ease: Power3.easeIn,opacity: 0, x: 100, scale:1.5})
@@ -7,15 +11,21 @@ function HelpAnimation() {
 
   this.scroller = scrollama();
   this.scroller.setup({
-      step: '.help-animation-container',
-      offset: 0.95,
-      progress: true,
-      threshold: 1
-    })
-    .onStepProgress(event => {
-      this.timeline.pause();
-      this.timeline.progress(event.progress);
-    });
+    step: '.help-animation-container',
+    offset: 0.95,
+    progress: true,
+    threshold: 1
+  })
+  .onStepProgress(event => {
+    this.timeline.pause();
+    this.timeline.progress(event.progress);
+  });
+
+  this.scroller.setup({
+     step: '.omega-help--title-subtitle',
+     offset: 0.75,
+   })
+   .onStepEnter(handleStepEnter);
 }
 
 export default  HelpAnimation;
